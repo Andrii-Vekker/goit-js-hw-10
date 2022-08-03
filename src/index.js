860c3dfc0a350277233d5cfae3d1aef2464ccdf5
Andrii-Vekker <65786577+Andrii-Vekker@users.noreply.github.com>
2022-08-03 16:29:17 +0300

    fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/styles.css';
-
+var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    input: document.querySelector("#search-box"),
+    list: document.querySelector(".country-list"),
+    info: document.querySelector(".country-info")
+}
+
+const nameCountry = "ukraine"
+
+
+
+
+fetch(`https://restcountries.com/v3.1/name/${nameCountry}?fields=name.official,capital,population,flags.svg,languages`)
+.then(response => response.json).then(data => console.log(data))
