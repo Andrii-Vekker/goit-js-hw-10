35faa4677627ceed335a94b3a5848a1c23520ff6
Andrii-Vekker <65786577+Andrii-Vekker@users.noreply.github.com>
2022-08-05 14:56:56 +0300

    we

--- src/index.js
+++ src/index.js
@@ -30,7 +30,13 @@ function inputHandler(e) {
 
 function fetchCountries(name) {
 return fetch(`https://restcountries.com/v3/name/${name}?fields=name,capital,population,flags,languages`)
-        .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error( Notify.failure("Oops, there is no country with that name"));
+           
+        }
+       return response.json()
+    })
 }
 
 function createlist(arr) {
